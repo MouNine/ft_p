2ef6a3041f0d44f800e8b997f5b37143c85416ac
Wagou <eboeuf@student.42.fr>
2015-03-28 14:50:45 +0100

    correction librairie client et serveur

--- client.c
+++ client.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+Freedom of expression is like the air we breathe, we don't feel it, until people/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
@@ -10,96 +10,47 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef __cplusplus
-#error Be sure you are using a C compiler...
-#endif
-
-//#if defined (WIN32) || defined (_WIN32)
-
-//#elif defined (linux) || defined (_POSIX_VERSION) || defined (_POSIX2_C_VERSION)\
- || defined (_XOPEN_VERSION)
-
-#include <winsock2.h>
-#include <sys/types.h>
-//#include <sys/socket.h>
-//#include <netinet/in.h>
-//#include <arpa/inet.h>
-#include <unistd.h> /* close */
-
-//#include <arpa/inet.h> //htons
-//#include <netinet/in.h> // struct sockaddr_in
-//#include <stdio.h>
-//#include <sys/socket.h>
-//#include "libft/includes/libft.h"
+#include <arpa/inet.h> //htons
+#include <netdb.h> // struct protoent
+#include <netinet/in.h> // struct sockaddr_in
+#include <stdio.h>
+#include <sys/socket.h>
+#include "libft/includes/libft.h"
 
 // chacun a une @, chacun se connecte @ (pour localiser l ordinateur)
-// et un port (parler a un service en particulier, exemple http)
-
-void        ft_putchar_fd(char c, int fd)
+// et un port (parler a un service en particulier, exemple http
+void                                    usage(char *str)
 {
-    write(fd, &c, 1);
-}
-
-void        ft_putstr_fd(char const *s, int fd)
-{
-    int     i;
-
-    i = 0;
-    while (s[i] != '\0')
-    {
-        ft_putchar_fd(s[i], fd);
-        i++;
-    }
-}
-
-void        ft_putendl_fd(char const *s, int fd)
-{
-    int     i;
-
-    i = 0;
-    if (s != NULL)
-    {
-        while (s[i] != '\0')
-        {
-            ft_putchar_fd(s[i], fd);
-            i++;
-        }
-        ft_putchar_fd('\n', fd);
-    }
-}
-
-void					usage(char *str)
-{
-	ft_putstr_fd("Usage: ", 2);
-	ft_putstr_fd(str, 2);
-	ft_putendl_fd(" <addr> <port>", 2);
-	exit(-1);
+        ft_putstr_fd("Usage: ", 2);
+        ft_putstr_fd(str, 2);
+        ft_putendl_fd(" <addr> <port>", 2);
+        exit(-1);
 }
 // initialisation :
 // - ouverture d'un socket en mode flux TCP/IP : socket(...)
 // - connexion : connect()
-int						create_client(char *addr, int port)
+int                                             create_client(char *addr, int port)
 {
-	int					sock;
-	struct protoent		*proto;
-	struct sockaddr_in	sin;	// adresse internet
+        int                                     sock;
+        struct protoent         *proto;
+        struct sockaddr_in      sin;    // adresse internet
 
-	proto = getprotobyname("tcp");
-	if (proto == 0)
-		return (-1);
-	sock = socket(PF_INET, SOCK_STREAM, proto->p_proto); // PF_INET : Internet v4, SOCK_STREAM : protocole connecte (ouvre un canal de communication pour parler à l'intérieur / different du coup par coup, sms par exemple)
-														// p_proto : numero de protocole de TCP
-	// association numero de port et adresse
-	sin.sin_family = AF_INET; //famille d'adresse internet
-	sin.sin_port = htons(port); // convertit une representation host vers une representation reseau sur un short (16 bits)
-	sin.sin_addr.s_addr = inet_addr(addr); // permet de convertir une chaine de caractere en adresse
-	// associe la socket a une adresse
-	if (connect(sock, (const struct sockaddr *)&sin, sizeof(sin)) == -1)
-	{
-		ft_putendl_fd("Connect error", 2);
-		exit(2);
-	}
-	return (sock);
+        proto = getprotobyname("tcp");
+        if (proto == 0)
+                return (-1);
+        sock = socket(PF_INET, SOCK_STREAM, proto->p_proto); // PF_INET : Internet v4, SOCK_STREAM : protocole connecte (ouvre un canal de communication pour parler à l'intérieur / different du coup par coup, sms par exemple)
+                                                                                                                // p_proto : numero de protocole de TCP
+        // association numero de port et adresse
+        sin.sin_family = AF_INET; //famille d'adresse internet
+		sin.sin_port = htons(port); // convertit une representation host vers une representation reseau sur un short (16 bits)
+        sin.sin_addr.s_addr = inet_addr(addr); // permet de convertir une chaine de caractere en adresse
+        // associe la socket a une adresse
+        if (connect(sock, (const struct sockaddr *)&sin, sizeof(sin)) == -1)
+        {
+                ft_putendl_fd("Connect error", 2);
+                exit(2);
+        }
+        return (sock);
 }
 // selon la demande :
 // - emettre des donnees : send()
@@ -111,18 +62,19 @@ int						create_client(char *addr, int port)
 //fin :
 // - deconnecter : shutdown()
 // - fermeture du socket : close()
-int						main(int ac, char **av)
+int                                             main(int ac, char **av)
 {
-	// recuperation du port dans les param
-	int	port;
-	int	sock; // fd (un peu different)
-	// si pas de param :
-	if (ac != 3)
-		usage(av[0]);
-	port = ft_atoi(av[2]);
-	// creation du client, renvoie une socket
-	sock = create_client(av[1], port);
-	write(sock, "bonjour\n", 8); // pour verifier que le serveur reçoit bien le message
-	close(sock);
-	return (0);
+        // recuperation du port dans les param
+        int     port;
+        int     sock; // fd (un peu different)
+        // si pas de param :
+        if (ac != 3)
+                usage(av[0]);
+        port = ft_atoi(av[2]);
+        // creation du client, renvoie une socket
+        sock = create_client(av[1], port);
+        write(sock, "bonjour\n", 8); // pour verifier que le serveur reçoit bien le message
+        close(sock);
+        return (0);
 }
+
--- server.c
+++ server.c
@@ -10,97 +10,55 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <winsock2.h>
-#include <sys/types.h>
+#include <arpa/inet.h> //htons
+#include <netdb.h> // struct protoent
+#include <netinet/in.h> // struct sockaddr_in
 #include <stdio.h>
-//#include <sys/socket.h>
-//#include <netinet/in.h>
-//#include <arpa/inet.h>
-#include <unistd.h> /* close */
-
-//#include <arpa/inet.h> //htons
-//#include <netdb.h> // struct protoent
-//#include <netinet/in.h> // struct sockaddr_in
-//#include <stdio.h>
-//#include <sys/socket.h>
-//#include "libft/includes/libft.h"
+#include <sys/socket.h>
+#include "libft/includes/libft.h"
 
 // chacun a une @, chacun se connecte @ (pour localiser l ordinateur)
 // et port (parler a un service en particulier, exemple http)
-
-void        ft_putchar_fd(char c, int fd)
+void            usage(char *str)
 {
-    write(fd, &c, 1);
-}
-
-void        ft_putstr_fd(char const *s, int fd)
-{
-    int     i;
-
-    i = 0;
-    while (s[i] != '\0')
-    {
-        ft_putchar_fd(s[i], fd);
-        i++;
-    }
-}
-
-void        ft_putendl_fd(char const *s, int fd)
-{
-    int     i;
-
-    i = 0;
-    if (s != NULL)
-    {
-        while (s[i] != '\0')
-        {
-            ft_putchar_fd(s[i], fd);
-            i++;
-        }
-        ft_putchar_fd('\n', fd);
-    }
-}
-
-void					usage(char *str)
-{
-	ft_putstr_fd("Usage: ", 2);
-	ft_putstr_fd(str, 2);
-	ft_putendl_fd(" <port>", 2);
-	exit(-1);
+        ft_putstr_fd("Usage: ", 2);
+        ft_putstr_fd(str, 2);
+        ft_putendl_fd(" <port>", 2);
+        exit(-1);
 }
 // initialisation :
 // - ouverture d'un socket en mode flux (TCP/IP) : socket(...)
 // - configurer adresse et port : bind()
 // - configurer nombre d'ecoute : listen()
-int						create_server(int port)
+int                                             create_server(int port)
 {
-	int					sock;
-	struct protoent		*proto;
-	struct sockaddr_in	sin;	// adresse internet
+        int                                     sock;
+        struct protoent         *proto;
+        struct sockaddr_in      sin;    // adresse internet
 
-	proto = getprotobyname("tcp");
-	if (proto == 0)
-		return (-1);
-	if ((sock = socket(PF_INET, SOCK_STREAM, proto->p_proto)) == -1) // PF_INET : Internet v4, SOCK_STREAM : protocole connecte (ouvre un canal de communication pour parler à l'intérieur / different du coup par coup, sms par exemple)
-																	// p_proto : numero de protocole de TCP
-	{
-		ft_putendl_fd("Socket error", 2);
-		exit(2);
-	}
-	printf ("socket %d is now opened in TCP/IP mode\n", sock);
-	// association numero de port et adresse
-	sin.sin_family = AF_INET; //famille d'adresse internet
-	sin.sin_port = htons(port); // convertit une representation host vers une representation reseau sur un short (16 bits)
-	sin.sin_addr.s_addr = htonl(INADDR_ANY); // 32bits
-	// associe la socket a une adresse
-	if (bind(sock, (const struct sockaddr *)&sin, sizeof(sin)) == -1)
-	{
-		ft_putendl_fd("Bind error", 2);
-		exit(2);
-	}
-	listen(sock, 42); // donne le nombre de connexions simultanees qu'on peut recevoir
-	printf("listening on port %d...\n", port);
-	return (sock);
+        proto = getprotobyname("tcp");
+        if (proto == 0)
+                return (-1);
+        if ((sock = socket(PF_INET, SOCK_STREAM, proto->p_proto)) == -1) // PF_INET : Internet v4, SOCK_STREAM : protocole connecte (ouvre un canal de communication pour parler à l'intérieur / different du coup par coup, sms par exemple)
+                                                                         // p_proto : numero de protocole de TCP
+ {
+                ft_putendl_fd("Socket error", 2);
+                exit(2);
+        }
+        printf ("socket %d is now opened in TCP/IP mode\n", sock);
+        // association numero de port et adresse
+        sin.sin_family = AF_INET; //famille d'adresse internet
+        sin.sin_port = htons(port); // convertit une representation host vers une representation reseau sur un short (16 bits)
+        sin.sin_addr.s_addr = htonl(INADDR_ANY); // 32bits
+        // associe la socket a une adresse
+        if (bind(sock, (const struct sockaddr *)&sin, sizeof(sin)) == -1)
+        {
+                ft_putendl_fd("Bind error", 2);
+                exit(2);
+        }
+        listen(sock, 42); // donne le nombre de connexions simultanees qu'on peut recevoir
+        printf("listening on port %d...\n", port);
+        return (sock);
 }
 // dans une boucle :
 // - accepter une connexion : accept()
@@ -113,64 +71,65 @@ int						create_server(int port)
 // - creer un thread avec le socket client
 // fin :
 // - fermeture du socket : close()
-int						main(int ac, char **av)
+int                                             main(int ac, char **av)
 {
-	// recuperation du port dans les param
-	int	port;
-	int	sock; // fd (un peu different)
-	int	cs; // client socket
-	unsigned int		cslen;
-	struct sockaddr_in	csin;
-	int 				r;
-	char				buf[1024];
-//	unsigned char 		data[128 + 1];
-	// si pas de param :
-	if (ac != 2)
-		usage(av[0]);
-	port = ft_atoi(av[1]);
-	// creation du serveur sur le port, renvoie une socket
-	sock = create_server(port);
-	printf("waiting for a client connection on port %d...\n", port);
-	while (42)
-	{
-		cslen = (unsigned int) sizeof csin;
-		cs = accept(sock, (struct sockaddr*)&csin, &cslen); // appel systeme, recreer une socket qui permet de faire discuter le serveur avec le client
-		if (cs != -1)
-			printf("client connected with socket %d from %s:%d\n", cs, inet_ntoa(csin.sin_addr), htons(csin.sin_port));
-		if (fork() == -1)
-			printf("Fork failed\n");
-		else if (fork() == 0)
-		{
-//	while ((r = recv(cs, data, (int) sizeof data - 1, 0)) > 0)
-//	{
-//		size_t nb_rec = (size_t) r;
-//		if (nb_rec > 0)
-//		{
-			/* convert to string */
- //       	data[nb_rec] = 0;
-//        	printf ("%u byte%s received:\n%s\n",(unsigned) nb_rec, nb_rec > 1 ? "s" : "",data);
+        // recuperation du port dans les param
+        int     port;
+        int     sock; // fd (un peu different)
+        int     cs; // client socket
+        unsigned int            cslen;
+        struct sockaddr_in      csin;
+        int                             r;
+        char                            buf[1024];
+//      unsigned char           data[128 + 1];
+        // si pas de param :
+        if (ac != 2)
+                usage(av[0]);
+        port = ft_atoi(av[1]);
+ // creation du serveur sur le port, renvoie une socket
+        sock = create_server(port);
+        printf("waiting for a client connection on port %d...\n", port);
+        while (42)
+        {
+                cslen = (unsigned int) sizeof csin;
+                cs = accept(sock, (struct sockaddr*)&csin, &cslen); // appel systeme, recreer une socket qui permet de faire discuter le serveur avec le client
+                if (cs != -1)
+                        printf("client connected with socket %d from %s:%d\n", cs, inet_ntoa(csin.sin_addr), htons(csin.sin_port));
+                if (fork() == -1)
+                        printf("Fork failed\n");
+                else if (fork() == 0)
+                {
+//      while ((r = recv(cs, data, (int) sizeof data - 1, 0)) > 0)
+//      {
+//              size_t nb_rec = (size_t) r;
+//              if (nb_rec > 0)
+//              {
+                        /* convert to string */
+ //             data[nb_rec] = 0;
+//              printf ("%u byte%s received:\n%s\n",(unsigned) nb_rec, nb_rec > 1 ? "s" : "",data);
  //       }
 //        else
 //            printf("client is disconnected\n");
-//	}
-			while ((r = read(cs, buf, 1023)) > 0)
-			{
-				sleep(2);
-				buf[r] = '\0';
-				printf("received %d bytes: [%s]\n", r, buf);
-			}
-			printf ("closing client socket %d...\n", cs);
-			close(cs);
-			exit(0);
-		}
-		else
-		{
-			printf ("closing client socket %d...\n", cs);
-			close(cs);
-		}
-	}
-	printf("closing socket %d...\n", sock);
-	close(sock);
-	printf("the socket is now closed\n");
-	return (0);
+//      }
+                        while ((r = read(cs, buf, 1023)) > 0)
+                        {
+                                sleep(2);
+                                buf[r] = '\0';
+                                printf("received %d bytes: [%s]\n", r, buf);
+                        }
+                        printf ("closing client socket %d...\n", cs);
+                        close(cs);
+                        exit(0);
+                }
+                else
+                {
+                        printf ("closing client socket %d...\n", cs);
+                        close(cs);
+                }
+        }
+        printf("closing socket %d...\n", sock);
+        close(sock);
+		printf("the socket is now closed\n");
+        return (0);
 }
+                     
